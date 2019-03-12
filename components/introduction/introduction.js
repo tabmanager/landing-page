7858e7a2c270314b6227070bd4a4549c4cb35a6e
Duc Vu <de.yd315@gmail.com>
2019-03-12 13:06:51 -0500

    added content json, and renderer scripts

--- components/introduction/introduction.js
+++ components/introduction/introduction.js
@@ -10,14 +10,15 @@ class Introduction extends Section{
         h1.appendChild(title);
 
         const h3 = document.createElement("h3");        
-        const title = document.createTextNode(this.subtitle);
-        h3.appendChild(title);
+        const subtitle = document.createTextNode(this.subtitle);
+        h3.appendChild(subtitle);
 
         const p = document.createElement("p");        
         const description = document.createTextNode(this.description);
         p.appendChild(description);
 
         const feature = document.createElement("section");
+        feature.appendChild(h1);
         feature.appendChild(h3);
         feature.appendChild(p);
         feature.classList.add("introduction");
